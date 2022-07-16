4480c8740a64ccfeddff1f60e699d56855acf872
Amit <amit@indihood.com>
2022-07-16 20:00:52 +0530

    settings

--- vendingmachine/settings.py
+++ vendingmachine/settings.py
@@ -83,8 +83,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'django.middleware.security.SecurityMiddleware'
 ]
 
 ROOT_URLCONF = 'vendingmachine.urls'
